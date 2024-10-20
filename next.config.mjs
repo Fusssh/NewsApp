df315c2aee65b096851d7e331913d2d51cf4a102
suman kumar pradhan <pradhank147@hotmail.com>
2024-10-20 07:02:48 +0530

    changet

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['media.wired.com'], // Add the allowed domains here
+  eslint: {
+    ignoreDuringBuilds: true,
     },
   };
   
